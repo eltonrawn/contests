ee8aabaee718f26d44c7d8d21f88c5c89fc6e3f7
rawnak <eltonrawn@gmail.com>
2021-04-21 12:17:12 +0600

    Update and minimize template

--- code-template/template.cpp
+++ code-template/template.cpp
@@ -2,31 +2,23 @@
 
 using namespace std;
 
-#define FOR(i, j, k) for(int i = j; i <= k; i++)
-#define ROF(i, j, k) for(int i = j; i >= k; i--)
-#define PB push_back
-#define MEM(n, val) memset((n), val, sizeof(n))
-#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define F first
-#define S second
-#define MP make_pair
-#define LL long long
 #define MOD 998244353
-#define MX 300010
-#define INFL 1000000000000000000LL
-#define INF 1000000000
+#define SZ 300010
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+typedef long long LL;
 typedef pair<int, int> PII;
 typedef pair<LL, LL> PLL;
 
 int main() {
 //    freopen("in.txt", "r", stdin);
 //    freopen("out.txt", "w", stdout);
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    FastIO;
+    
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     int tc;
     cin >> tc;
-    FOR(tt, 1, tc) {
+    for(int tt = 1; tt <= tc; tt++) {
+
     }
     return 0;
 }
