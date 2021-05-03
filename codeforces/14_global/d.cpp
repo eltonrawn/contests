6c0d2fe9a587ceb23a003d29d95c600a082aaaf5
rawnak <eltonrawn@gmail.com>
2021-05-03 17:51:34 +0600

    Update d cf_14_global

--- codeforces/14_global/d.cpp
+++ codeforces/14_global/d.cpp
@@ -13,18 +13,15 @@ typedef pair<LL, LL> PLL;
 class cmp1 {
 public:
     bool operator()(PII a, PII b) {
-        if(a.first == b.first) {
+        if(a.first == b.first)
             return a.second < b.second;
-        }
         return a.first > b.first;
     }
 };
 
 int n, l, r;
-// int ara[SZ];
-// vector<int> lvec, rvec;
 map<int, int> lmp, rmp;
-set<PII, cmp1> lst, rst;
+set<PII, cmp1> rst;
 
 int main() {
 //    freopen("in.txt", "r", stdin);
@@ -36,8 +33,7 @@ int main() {
     for(int tt = 1; tt <= tc; tt++) {
         cin >> n >> l >> r;
         // lvec.clear();rvec.clear();
-        lmp.clear();rmp.clear();
-        lst.clear(); rst.clear();
+        lmp.clear();rmp.clear();rst.clear();
         LL ans = 0;
 
         int lcnt = 0;
@@ -45,56 +41,32 @@ int main() {
 
         for(int i = 1; i <= l; i++) {
             int tmp;cin >> tmp;
-            // lvec.push_back(tmp);
-            lmp[tmp]++;
-            lcnt++;
+            lmp[tmp]++;lcnt++;
         }
         for(int i = 1; i <= r; i++) {
             int tmp;cin >> tmp;
             if(lmp[tmp]) {
-                lmp[tmp]--;
-                lcnt--;
+                lmp[tmp]--;lcnt--;
                 continue;
             }
-            // rvec.push_back(tmp);
-            rmp[tmp]++;
-            rcnt++;
+            rmp[tmp]++;rcnt++;
         }
 
-        // if(lvec.size() > rvec.size()) {
-        //     swap(lvec, rvec);   
-        //     swap(lmp, rmp);
-        // }
-
-        for(auto it = lmp.begin(); it != lmp.end(); it++) {
-            lst.insert({it->second, it->first});
+        if(lcnt > rcnt) {
+            swap(lmp, rmp);
+            swap(lcnt, rcnt);
         }
 
-        for(auto it = rmp.begin(); it != rmp.end(); it++) {
+        for(auto it = rmp.begin(); it != rmp.end(); it++)
             rst.insert({it->second, it->first});
-        }
 
-        while(lcnt != rcnt) {
-            if(lcnt > rcnt) {
-                if((*lst.begin()).first < 2) {
-                    break;
-                }
-                PII pr = *lst.begin();lst.erase(lst.begin());
-                pr.first -= 2;
-                lst.insert(pr);
-                lcnt -= 2;
-                ans++;
-            }
-            else if(rcnt > lcnt) {
-                if((*rst.begin()).first < 2) {
-                    break;
-                }
-                PII pr = *rst.begin();rst.erase(rst.begin());
-                pr.first -= 2;
-                rst.insert(pr);
-                rcnt -= 2;
-                ans++;
-            }
+        while(rcnt > lcnt) {
+            if((*rst.begin()).first < 2)break;
+            PII pr = *rst.begin();rst.erase(rst.begin());
+            pr.first -= 2;
+            rst.insert(pr);
+            rcnt -= 2;
+            ans++;
         }
         // cout << lcnt << " " << rcnt << endl;
         LL tot = lcnt + rcnt;
