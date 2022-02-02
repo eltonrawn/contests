eb95b5fd1f30cac4b0d542af744b0277b11c2c6c
rawnak <eltonrawn@gmail.com>
2022-02-02 11:25:49 +0600

    Solve a cf_628_div2

---
+++ codeforces/628_div2/a.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 998244353
+#define SZ 300010
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+
+int x;
+
+int main() {
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int tc;
+    cin >> tc;
+    for(int tt = 1; tt <= tc; tt++) {
+        cin >> x;
+        cout << x - 1 << " " << 1 << endl;
+    }
+    return 0;
+}
+/**
+*/
